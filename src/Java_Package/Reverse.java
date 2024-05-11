109a6919f1d7d79afc207b76a5343c6f19eac9e0
priti <pritibhushanf@gmail.com>
2024-05-11 08:30:10 +0530

    11may_reversestring

---
+++ src/Java_Package/Reverse.java
@@ -0,0 +1,25 @@
+package Java_Package;
+
+public class Reverse {
+	public static void main(String[] args) {
+		//convert a given string in to reverse string
+		
+		//using String
+		String givenString = "4568hjh";
+		String revString ="";
+		
+		for(int i=0;i<=givenString.length()-1;i++)
+		{
+			revString =  givenString.charAt(i) +revString;
+		}
+		
+		System.out.println(revString);
+		
+		
+		//using string buffer
+		
+		
+		//using string builder
+	}
+
+}
