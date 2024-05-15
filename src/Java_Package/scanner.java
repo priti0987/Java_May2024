ac0216486a231127120aab3e21a29ffc32182dee
priti <pritibhushanf@gmail.com>
2024-05-15 12:32:57 +0530

    15May2024

---
+++ src/Java_Package/scanner.java
@@ -0,0 +1,18 @@
+package Java_Package;
+
+import java.util.Scanner;
+
+public class scanner {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Enter name :");
+
+		String myString = sc.next();
+		
+		System.out.println("name is : "+ myString);
+		
+	}
+
+}
